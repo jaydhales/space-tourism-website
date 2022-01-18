0182411e4e56c1528f0422513ed78ef75d5504a3
james <jaydhales@gmail.com>
2022-01-18 17:44:17 +0100

    .

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -11,12 +11,10 @@ function Home() {
           <div className="details">
             <h1>Space</h1>
             <p>
-              Let’s face it; if you want to go to space, you might as well{" "}
-              <br />
-              genuinely go to outer space and not hover kind of on the <br />
-              edge of it. Well sit back, and relax because we’ll give you a{" "}
-              <br />
-              truly out of this world experience!
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
             </p>
           </div>
           <Link to="/destination" className="landing-page-main-btn">
